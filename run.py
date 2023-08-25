b38bf303aa3ab7390849f0b2b6fa5acfb8be5528
TomLiDev <>
2023-08-25 10:49:51 +0000

    Update creat_board function to add grid letters on board left

--- run.py
+++ run.py
@@ -1,82 +1,86 @@
 import random
 
+
 SHIPS = [
-    {"Name": "Battleship", "Map_value": 4, "Size": 4},
-    {"Name": "Cruiser", "Map_value": 3, "Size": 3},
-    {"Name": "Destroyer", "Map_value": 2, "Size": 2}
+    {"name": "Battleship", "value": 4},
+    {"name": "Cruiser", "value": 3}
 ]
+
 
 def place_ship(board):
     """
-    Function to loop through the ships list of dicts and place them
-    on the board for both human player and computer
+    Function which places ships on the gameboard, uses ship value in dict
+    to determine length
     """
-    print("Ships board", board)
-    print(SHIPS)
-    print(SHIPS[1])
-    ship_row = random.randrange(0, 10)
-    print("ship row", ship_row)
-    ship_start = random.randrange(0, 10)
-    print("ship start", ship_start)
-    print("ship value", SHIPS["Map_value"])
+    print("board in place function", board)
+    print("ship value", SHIPS[0]["value"])
+    
+    row = board[2]
+    print(board[2])
+    print(board)
+    print(len(board))
 
-    for ship in SHIPS:
-        board[ship_row] = ["Map_value"]
-        print(board[ship_row])
+    columns = [i for i in range(0, len(board)+1)]
+    print(columns)
+    board.insert(0, columns)
+    print(board)
 
-def create_board(userboard):
+
+def create_board(boardsize):
     """
-    Function which creates the game board based on user selection. 
-    Game board is a list of lists populated with zero's initially 
-    to represent empty open sea. Game board either 6x6 or 10x10 
+    Function which creates the game board based on user selection. Game board 
+    is a list of lists populated with zero's initially to represent empty
+    open sea. Game board either 6x6 or 10x10 
     based on user selection.
     """
+    print("Game board creation")
     board = []
     row = []
-    if userboard == 10:
-
+    letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    if boardsize == 10:
         while len(board) < 10:
             while len(row) < 10:
                 row.append(0)
             print(row)
             board.append(row)
         print("LargeBoard", board)
-    else:
-        while len(board) < 6:
-            while len(row) < 6:
-                row.append(0)
-            print(row)
-            board.append(row)
-        print("SmallBoard", board)
-    
-    place_ship(board)
+
+    if boardsize == 6:
+        board = [[0] * 6 for i in range(6)]
+        print("list board", board)
+        for i in range(len(board)):
+            board[i].insert(0, letters[i])
+            print(board[i])
+        print(board)
     return board
 
 
-def game_select ():
+def game_select():
     """
     Function which displays welcome message and gets
     initial choice from user on board size
     """
-    print("Welcome to Battleships!")
-    print("Please select a board size")
+    print("Please select a game board size")
     print("6 = 6x6 game board")
     print("10 = 10x10 game board")
     
     userinput = input("Enter 6 or 10: ")
 
     print(userinput)
+    print(type(userinput))
 
-    if userinput == "10":
-        boardsize = 10
-        create_board(boardsize)
     if userinput == "6":
         boardsize = 6
+        print("boardsize", boardsize)
+        create_board(boardsize)
+    elif userinput == "10":
+        boardsize = 10
+        print("boardsize", boardsize)
         create_board(boardsize)
     else:
-        print("Please enter either 10 or 6")
+        print(f"Sorry {userinput} isn't valid, please enter either 10 or 6")
         game_select()
 
-    return boardsize
 
+print("Welcome to Battleships!")
 game_select()
